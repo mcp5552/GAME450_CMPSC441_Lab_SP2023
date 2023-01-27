8f291589c9daf61053d3f2357c9803fb184b0b80
mcp5552 <mcp5552@psu.edu>
2023-01-27 10:38:27 -0500

    Implemented bresenham

--- src/lab3/travel_cost.py
+++ src/lab3/travel_cost.py
@@ -8,6 +8,7 @@ In this lab, you will write a function that calculates the cost of a route betwe
 A terrain is generated for you 
 '''
 import numpy as np
+from bresenham import bresenham
 
 def get_route_cost(route_coordinate, game_map):
     """
@@ -39,7 +40,7 @@ def get_route_cost(route_coordinate, game_map):
     :return: a floating point number representing the cost of the route
     """
     # Build a path from start to end that looks like [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 4)]
-    pass 
+    path = list(bresenham(route_coordinate[0][0],route_coordinate[0][1],route_coordinate[1][0],route_coordinate[1][1]))
     return game_map[tuple(zip(*path))].sum()
 
 

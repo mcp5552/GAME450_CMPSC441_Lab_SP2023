ca2abeb0095747b201218abe3321f044c98d95c2
mcp5552 <mcp5552@psu.edu>
2023-03-26 22:05:06 -0400

    Sunday night

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -8,15 +8,17 @@ class PyGameAIPlayer:
         pass
 
     def selectAction(self, state):
-        return random.choice(['0','1','2','3','4','5','6','7', '8', '9'])
+        return random.choice([ord("0"),ord("1"),ord("2"),ord("3"),ord("4"),ord("5"),ord("6"),ord("7"),ord("8"),ord("9")])
 
 
 """ Create PyGameAICombatPlayer class here"""
-
-
-class PyGameAICombatPlayer:
+class PyGameAICombatPlayer(CombatPlayer):
     def __init__(self, name):
         super().__init__(name)
 
     def weapon_selecting_strategy(self):
-        return random.choice([0,1,2])
+        print("selecting weapon") #debug
+        while True:
+            self.weapon = random.randint(0, 2)
+            print("choice is: " + self.weapon) #debug
+            return self.weapon
